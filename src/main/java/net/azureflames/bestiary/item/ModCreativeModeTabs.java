9e0f1870012f6bcb79ea23f274bdc773643a0d2b
ScottSummers1 <scottsummers1@ymail.com>
2023-06-23 14:46:44 -0500

    Found the missing piece, creative tab is working now

--- src/main/java/net/azureflames/bestiary/item/ModCreativeModeTabs.java
+++ src/main/java/net/azureflames/bestiary/item/ModCreativeModeTabs.java
@@ -6,12 +6,14 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Bestiary.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModCreativeModeTabs {
     public static CreativeModeTab BESTIARY_TAB;
 
+    @SubscribeEvent
     public static void registerCreativeModeTabs(CreativeModeTabEvent.Register event) {
         BESTIARY_TAB = event.registerCreativeModeTab(new ResourceLocation(Bestiary.MOD_ID, "bestiary_tab"),
                 builder -> builder.icon(() -> new ItemStack(ModItems.BLACK_OPAL.get()))
