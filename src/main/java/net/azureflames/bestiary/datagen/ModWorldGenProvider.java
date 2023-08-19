933a82eb26041e0a42628482977cd315d1973b54
ScottSummers1 <scottsummers1@ymail.com>
2023-08-19 15:17:23 -0500

    Adding Ore Generation

--- src/main/java/net/azureflames/bestiary/datagen/ModWorldGenProvider.java
+++ src/main/java/net/azureflames/bestiary/datagen/ModWorldGenProvider.java
@@ -2,6 +2,7 @@ package net.azureflames.bestiary.datagen;
 
 import net.azureflames.bestiary.Bestiary;
 import net.azureflames.bestiary.worldgen.ModConfiguredFeatures;
+import net.azureflames.bestiary.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
@@ -13,7 +14,8 @@ import java.util.concurrent.CompletableFuture;
 
 public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
-            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap);
 
     public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(Bestiary.MOD_ID));
