1cb531fb5672c1193052b1ed539c163aec311485
ScottSummers1 <scottsummers1@ymail.com>
2023-08-18 20:44:04 -0500

    Adding Trees and soon tree generation Part 2 Adding sapling json fixed

--- src/main/java/net/azureflames/bestiary/datagen/ModItemModelProvider.java
+++ src/main/java/net/azureflames/bestiary/datagen/ModItemModelProvider.java
@@ -1,10 +1,12 @@
 package net.azureflames.bestiary.datagen;
 
 import net.azureflames.bestiary.Bestiary;
+import net.azureflames.bestiary.block.ModBlocks;
 import net.azureflames.bestiary.item.ModItems;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -19,8 +21,14 @@ public class ModItemModelProvider extends ItemModelProvider {
     protected void registerModels() {
         simpleItem(ModItems.BLACK_OPAL);
         simpleItem(ModItems.RAW_BLACK_OPAL);
+        saplingItem(ModBlocks.EBONY_SAPLING);
     }
 
+    private ItemModelBuilder saplingItem(RegistryObject<Block> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(Bestiary.MOD_ID,"block/" + item.getId().getPath()));
+    }
     private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/generated")).texture("layer0",
